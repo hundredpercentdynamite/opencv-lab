6fbdb2cfb82c00e6855f4503042c10113422df0b
Vladimir Kleymenov <vladimir.kleymenov@rtlabs.ru>
2022-02-08 12:20:00 +0300

    Доделана первая лаба

--- prj.labs/lab01/lab01.cpp
+++ prj.labs/lab01/lab01.cpp
@@ -23,9 +23,22 @@ int main() {
   img = 0;
   drawGradient(img, 0, 0);
 
-  // draw gamma
+
+  // draw gamma with pow
   drawGradient(img, 0, 60);
-  cv::Rect2d rc2 = {0, 60, 768, 60 };
+  cv::Rect2d rc3 = {0, 60, 768, 60 };
+  img.convertTo(img, CV_32F);
+  img = img / 255;
+  cv::Mat thirdrect = img(rc3);
+  cv::Mat tmp;
+  cv::pow(thirdrect, 2.2, tmp);
+  tmp.copyTo(thirdrect);
+  img = img * 255;
+  img.convertTo(img, CV_8UC1);
+
+  // draw gamma
+  drawGradient(img, 0, 120);
+  cv::Rect2d rc2 = {0, 120, 768, 60 };
   cv::Mat secondrect = img(rc2);
   for (int i = 0; i <= rc2.height; i++) {
     for (int j = 0; j <= rc2.width; j++) {
@@ -34,17 +47,6 @@ int main() {
       point = cv::saturate_cast<u_char>(tmp);
     }
   }
-
-
-  // draw gamma with pow
-//  drawGradient(img, 0, 120);
-//  cv::Rect2d rc3 = {0, 120, 768, 60 };
-//  img.convertTo(img, CV_32F);
-//  cv::Mat thirdrect = img(rc3);
-//  cv::Mat tmp;
-//  cv::pow(thirdrect, 2.2, tmp);
-//  tmp.copyTo(thirdrect);
-//  img.convertTo(img, CV_8UC1);
 
   //   save result
   cv::imwrite("lab01.png", img);
