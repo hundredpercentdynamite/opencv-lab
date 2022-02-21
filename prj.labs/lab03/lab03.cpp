d6ed9ae059cc90e6df48055766b275e5a7cd3f38
Vladimir Kleymenov <vladimir.kleymenov@rtlabs.ru>
2022-02-21 13:52:19 +0300

    lab03 done

---
+++ prj.labs/lab03/lab03.cpp
@@ -0,0 +1,65 @@
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+/// Функция  y = (sin(exp(x^2))^3 + x) / 2
+double func(double x) {
+  double exp = std::exp(x * x);
+  double sin = std::sin(exp);
+
+  return (sin * sin * sin + x) / 2;
+}
+
+cv::Mat drawFunc(std::vector<double>& xCords, std::vector<double>& yCords) {
+  double size = 512;
+  cv::Mat graph = cv::Mat::ones((int)size, (int)size, CV_8UC1) * 255;
+
+  for (int i = 0; i < xCords.size(); ++i) {
+    double prevXCord = xCords[i - 1];
+    double prevYCord = yCords[i - 1];
+    double xCord = xCords[i];
+    double yCord = yCords[i];
+    cv::Point2d prevPoint(size * prevXCord, size - (size * prevYCord));
+    cv::Point2d point(size * xCord, size - (size * yCord));
+    line( graph, prevPoint, point, {0 }, 2, 8, 0);
+  }
+
+  return graph;
+}
+
+int main() {
+  auto imgPath = cv::samples::findFile("../data/cross_0256x0256.png");
+  cv::Mat img = cv::imread(imgPath, cv::IMREAD_COLOR);
+
+  std::vector<double> xCords(256);
+  std::vector<double> yCords(256);
+  std::vector<uint8_t> lutY(256);
+
+  for (int i = 0; i < 256; ++i) {
+    double xValue = (double)i / 255;
+    xCords[i] = xValue;
+    double yValue = func(xValue);
+    yCords[i] = yValue;
+    lutY[i] = cv::saturate_cast<uint8_t>(yValue * 255);
+  }
+
+
+  /// LUT
+  cv::Mat grayscale(256, 256, CV_8UC1);
+  cv::cvtColor(img, grayscale, cv::COLOR_BGR2GRAY);
+
+  cv::Mat grayscaleLut(256, 256, CV_8UC1);
+  cv::LUT(grayscale, lutY, grayscaleLut);
+
+  cv::Mat transformed(256, 256, CV_8UC3);
+  cv::LUT(img, lutY, transformed);
+
+  cv::Mat graph = drawFunc(xCords, yCords);
+
+  cv::imwrite("lab03_rgb.png", img);
+  cv::imwrite("lab03_gre.png", grayscale);
+  cv::imwrite("lab03_gre_res.png", grayscaleLut);
+  cv::imwrite("lab03_rgb_res.png", transformed);
+  cv::imwrite("lab03_viz_func.png", graph);
+
+  return 0;
+}
