c0449a71100e3a6c86ad15a10f976285705e57e4
Vladimir Kleymenov <vladimir.kleymenov@rtlabs.ru>
2022-02-21 14:23:32 +0300

    lab03 added axis on in graph

--- prj.labs/lab03/lab03.cpp
+++ prj.labs/lab03/lab03.cpp
@@ -10,17 +10,21 @@ double func(double x) {
 }
 
 cv::Mat drawFunc(std::vector<double>& xCords, std::vector<double>& yCords) {
-  double size = 512;
+  double size = 517;
   cv::Mat graph = cv::Mat::ones((int)size, (int)size, CV_8UC1) * 255;
-
-  for (int i = 0; i < xCords.size(); ++i) {
+  line(graph, cv::Point2d(0, 507), cv::Point2d(517, 507), {0}, 2, 8, 0);
+  line(graph, cv::Point2d(5, 517), cv::Point2d(5,  0), {0}, 2, 8, 0);
+  cv::putText(graph, "X", cv::Point2f(500, 500), 1, 1, { 0 });
+  cv::putText(graph, "Y", cv::Point2f(10, 15), 1, 1, { 0 });
+  cv::putText(graph, "0", cv::Point2f(7, 503), 1, 1, { 0 });
+  for (int i = 1; i < xCords.size(); ++i) {
     double prevXCord = xCords[i - 1];
     double prevYCord = yCords[i - 1];
     double xCord = xCords[i];
     double yCord = yCords[i];
-    cv::Point2d prevPoint(size * prevXCord, size - (size * prevYCord));
-    cv::Point2d point(size * xCord, size - (size * yCord));
-    line( graph, prevPoint, point, {0 }, 2, 8, 0);
+    cv::Point2d prevPoint(size * prevXCord + 5, size - (size * prevYCord) - 5);
+    cv::Point2d point(size * xCord + 5, size - (size * yCord) - 5);
+    line( graph, prevPoint, point, { 100 }, 2, 8, 0);
   }
 
   return graph;
@@ -61,5 +65,10 @@ int main() {
   cv::imwrite("lab03_rgb_res.png", transformed);
   cv::imwrite("lab03_viz_func.png", graph);
 
+  /// Display
+  namedWindow("calcHist Demo", cv::WINDOW_AUTOSIZE );
+  imshow("calcHist Demo", graph );
+
+  cv::waitKey(0);
   return 0;
 }
